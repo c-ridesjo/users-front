a628cec31aeb6140c66cd4bc0a4637eec222aa9e
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-03-07 10:52:33 +0100

    Corrective changes

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //imports, globala variabler och funktioner - den ordningen uppifrån och ned
 let userList = document.getElementById("userList");
 let newUser = document.getElementById("newUser");
+let newUserPassword = document.getElementById("newUserPassword");
 let saveUserBtn = document.getElementById("saveUserBtn");
-
+let loginUsername = document.getElementById("loginUsername");
+let loginPassword = document.getElementById("loginPassword");
+let loginUserBtn = document.getElementById("loginUserBtn");
+let logoutUserBtn = document.getElementById("logoutUserBtn");
+let loginMessage = document.getElementById("loginMessage");
 
 fetch("http://localhost:3000/users")    //hämtar datan. (Det här är en GET-fetch.)
 .then(res => res.json())                
@@ -10,6 +15,11 @@ fetch("http://localhost:3000/users")    //hämtar datan. (Det här är en GET-fe
     //console.log(data);
     printUsers(data);        //när datan är hämtad anropas den här funktionen
 });
+
+let loggedInUser = localStorage.getItem("username");
+if(loggedInUser) {
+    loginMessage.innerText ="Welcome!" + loggedInUser;
+}
 
 function printUsers(users) {
     console.log(users);
@@ -27,7 +37,7 @@ function printUsers(users) {
 saveUserBtn.addEventListener("click", () => {
 
     //skapa en ny användare
-    let user = {name: newUser.value, password: newUser.value};
+    let user = {name: newUser.value, password: newUserPassword.value};
     console.log(user);
 
     //skicka till servern
@@ -40,6 +50,48 @@ saveUserBtn.addEventListener("click", () => {
     })    
     .then(res => res.json())                
     .then(data => {
-        print.user(data);
+        printUsers(data);
     });    
-})
+
+    newUser.value = "";
+    newUserPassword.value = "";
+});
+
+loginUserBtn.addEventListener("click", () => {
+
+    //skapa en ny användare
+    let loginUser = {
+        name: loginUsername.value, 
+        password: loginPassword.value
+    }
+    
+    console.log(loginUser);
+
+    //skicka till servern
+    fetch("http://localhost:3000/users/login", {
+    method: "POST",         //hämtar datan. (Det här är en POST-fetch.)
+    headers: {
+        "Content-Type": "application/json",        
+    },
+    body: JSON.stringify(loginUser)      //body är det paketet som vi skickar
+    })    
+    .then(res => res.json())                
+    .then(data => {
+        console.log(data);
+        
+        if(data.name) {
+            loginMessage.innerText=`Welcome! ` + data.name;
+            localStorage.setItem("username", data.name);
+         } 
+         else {
+            loginMessage.innerText=`Incorrect username or password! Try again!`;
+        }
+    });
+    loginUsername.value = "";
+    loginPassword.value = "";
+});
+
+logoutUserBtn.addEventListener("click", () => {
+    localStorage.removeItem("username");
+    loginMessage.innerText = "You have been logged out!"
+});
